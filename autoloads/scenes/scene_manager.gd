ccda67658fb3ee33d82d456d899595be393df7f8
Rile-Cry <rileycriddle@yahoo.com>
2025-10-06 12:49:07 -0400

    HP Fix
    Made it so player currently one-shots enemy and also health resets between scenes.

--- autoloads/scenes/scene_manager.gd
+++ autoloads/scenes/scene_manager.gd
@@ -5,14 +5,6 @@ var prog_scene: ProgScene
 
 @export var scene_paths : Dictionary[StringName, PackedScene] = {}
 
-# AI: This is generated code, come back to it
-#region AI Generated
 func load_next_scene():
 	prog_scene.generate_next_events()
-
-func _on_scene_changed(scene_type: String, scene_data: Dictionary):
-	print("Scene changed to: ", scene_type, " with data: ", scene_data)
-
-func _load_scene(scene_data: Dictionary):
-	var scene_type = scene_data["scene_type"]
-#endregion
+	PlayerManager.hp = PlayerManager.max_hp
