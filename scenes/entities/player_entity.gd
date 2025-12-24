29fc4f28c70419bab81c75b7b68d496d1f603265
Clockknight <hello@tylerhanyinwang.com>
2025-12-24 15:46:17 -0800

    Add todos for how to add the player entity

--- scenes/entities/player_entity.gd
+++ scenes/entities/player_entity.gd
@@ -19,3 +19,21 @@ func update() -> void:
 
 #region Signal Callbacks
 #endregion
+
+
+
+# TODO implement this
+# have battle map populate this guy into a tile
+	#what is spawning the enemies
+	# how does it decide what to spawn
+	# and where
+	# make an additional spawn
+	# set it to center column, bottom row
+# use arrows to move for now
+	# movin	is moving data to target tile
+	# the clearing OG parent tile
+# Selectability of tiles should be decided by where the player is, instead of the bottom row of the map
+	# find where selectability is decided
+	# chang eit to find the tile with player?
+	# then base it off of that? Somehow?
+	# what about verticality?
