677f433915b68484a76c3fea8fea62c9e00a87d2
Clockknight <hello@tylerhanyinwang.com>
2026-01-07 14:41:24 -0800

    Itemholder layout

---
+++ utility/data_holders/item_holder.gd
@@ -0,0 +1,14 @@
+class_name ItemHolder extends Resource
+
+@export var item_id : StringName
+@export var item_name : String
+@export var has_action : bool
+@export var action_name : StringName
+@export var usable : bool
+@export var uses_left : int
+@export var equippable : bool
+@export var item_material : StringName
+@export var item_modifier : StringName
+@export var equip_slot : StringName
+@export var stackable : bool
+@export var stack_count : int
