bf435c7e21cf3f7889d6a456f89ca22df4646b74
Clockknight <hello@tylerhanyinwang.com>
2026-01-09 17:10:33 -0800

    Previously mentioned fenagling
    dict.get() seems to not work as intended

--- mechanics/crafting/recipe_compendium.gd
+++ mechanics/crafting/recipe_compendium.gd
@@ -8,15 +8,15 @@ func init() -> void:
 	if compendium_path:
 		var loaded_recipes = FileHelper.load_asset(compendium_path).records
 		for recipe in loaded_recipes:
-			var recipe_name = recipe.get(&"RecipeName").to_snake_case()
+			var recipe_name = recipe.get(recipe.find(&"RecipeName")).to_snake_case()
 			recipes[recipe_name] = {
-				&"tier": recipe.get(&"Tier", 0),
-				&"cloth": recipe.get(&"Cloth"),
-				&"leather": recipe.get(&"Leather"),
-				&"wood": recipe.get(&"Wood"),
-				&"metal": recipe.get(&"Metal"),
-				&"dust": recipe.get(&"Dust"),
-				&"core": recipe.get(&"Core"),
-				&"boss_material": recipe.get(&"BossMaterial"),
+				&"tier": recipe.get(recipe.find(&"Tier")),
+				&"cloth": recipe.get(recipe.find(&"Cloth")),
+				&"leather": recipe.get(recipe.find(&"Leather")),
+				&"wood": recipe.get(recipe.find(&"Wood")),
+				&"metal": recipe.get(recipe.find(&"Metal")),
+				&"dust": recipe.get(recipe.find(&"Dust")),
+				&"core": recipe.get(recipe.find(&"Core")),
+				&"boss_material": recipe.get(recipe.find(&"BossMaterial")),
 				&"discovered": false
 			}
