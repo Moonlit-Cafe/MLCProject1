09adc0ccd31f4fb79ee524f5989d11c9aeda474a
Clockknight <hello@tylerhanyinwang.com>
2026-01-23 17:42:57 -0800

    Completed turn tracker

--- ui/events/battle/turn_tracker.gd
+++ ui/events/battle/turn_tracker.gd
@@ -7,6 +7,29 @@ var turn_list : Array[TileEntity] = []
 # TODO: Rudimentary, but good start.
 func _ready() -> void:
 	GameGlobalEvents.battle_removed.connect(remove_turn)
+	
+## Orders turns of all entities from highest Haste to lowest, 
+## while also preserving the current turn
+## eg: a list that's normally [1,2,4,3]
+## looped around to [4,3,1,2]
+## Will sort around to [3,4,1,2]
+func reorder_turns() -> void:
+	if turn_list == []:
+		return
+	if validate_turns():
+		return
+	
+	var list_header = turn_list[0]
+	
+	turn_list.sort_custom(_compare_haste)
+	
+	# HACK potential computation pain point
+	# dont know if its faster to do this or rearrange slices of the array
+	# mostly depends on slice comp time
+	# bsearch would make this basically run at o(logn)
+	# this just runs at o(n)
+	while turn_list[0] != list_header:
+		recycle_turn()
 #endregion
 
 #region Helpers
@@ -20,10 +43,7 @@ func generate_turns() -> void:
 		turn_label.text = turn.character.o_name
 		turn_labels.add_child(turn_label)
 	
-	order_turns()
-	# TODO other funcs?
-	# should probably have a build turns
-		# populate list with all turns
+	turn_list.sort_custom(_compare_haste)
 
 func remove_turn(actor: TileEntity) -> void:
 	if not actor in turn_list:
@@ -42,60 +62,45 @@ func remove_turn(actor: TileEntity) -> void:
 func recycle_turn() -> void:
 	var turn = turn_labels.get_children().pop_front()
 	turn_labels.move_child(turn, turn_labels.get_child_count())
-	
-func validate_turn() -> void:
-	pass
-	# TODO implement this
-	# just scans through the turn order
-	# allows for one jump in haste
-	
-## Orders turns of all entities from highest Haste to lowest, 
-## while also preserving the current turn
-## eg: a list that's normally [1,2,4,3]
-## looped around to [4,3,1,2]
-## Will sort around to [3,4,1,2]
-func order_turns() -> void:
-	var new_list = []
-	
+
+
+func validate_turns() -> bool:
 	if turn_list == []:
-		return
-	# if turn_list_ordered:
-		# return
+		return true
 	
-	new_list = _turn_sublist(turn_list[0])
+	var cur_haste = turn_list[0].haste
+	var looped = false
 	
-	var second_list = []
-	while turn_list != []:
-		
-		
-	new_list.append_array(second_list)
+	for turn in turn_list:
+		if turn.haste > cur_haste:
+			if looped:
+				return false
+			else:
+				looped = true
+				continue
 	
-	self.turn_list = new_list
+	return true
 
-func init_turns() -> void:
-	order_turns()
+#func _turn_sublist(cur_turn) -> Array[TileEntity]:
+	#var new_list = []
+	#var cur_next = cur_turn.haste
+	#
+	#while turn_list != []:
+		#for turn in turn_list:
+			#if turn.haste == cur_turn.haste:
+				#turn_list.pop_at(turn_list.bsearch(turn))
+				#new_list.append(turn)
+				#
+			#elif turn.haste < cur_next:
+				#cur_next = turn.haste
+			#
+		#if cur_next == cur_turn.haste:
+			#break
+	#
+	#return new_list
 	
-	# TODO implement the rest of this
-	# find the first biggest one
-	# if its in the first spot, just return
-	# else, split into two arrays,
-
-
-func _turn_sublist(cur_turn) -> Array[TileEntity]:
-	var new_list = []
-	var cur_next = cur_turn.haste
-	
-	while turn_list != []:
-		for turn in turn_list:
-			if turn.haste == cur_turn.haste:
-				turn_list.pop_at(turn_list.bsearch(turn))
-				new_list.append(turn)
-				
-			elif turn.haste < cur_next:
-				cur_next = turn.haste
-			
-		if cur_next == cur_turn.haste:
-			break
-	
-	return new_list
+func _compare_haste(a, b) -> bool:
+	if a.haste < b.haste:
+		return true
+	return false
 #endregion
