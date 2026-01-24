59b24fa2369e4c9e4938c9eb0ddfb47866c60fd4
Clockknight <hello@tylerhanyinwang.com>
2026-01-23 16:22:33 -0800

    Part of turn ordering implemented

--- ui/events/battle/turn_tracker.gd
+++ ui/events/battle/turn_tracker.gd
@@ -3,9 +3,13 @@ class_name TurnTracker extends Control
 @export var turn_labels : VBoxContainer
 var turn_list : Array[TileEntity] = []
 
+#region Events
 # TODO: Rudimentary, but good start.
 func _ready() -> void:
 	GameGlobalEvents.battle_removed.connect(remove_turn)
+#endregion
+
+#region Helpers
 
 func generate_turns() -> void:
 	if not turn_labels:
@@ -39,30 +43,34 @@ func recycle_turn() -> void:
 	var turn = turn_labels.get_children().pop_front()
 	turn_labels.move_child(turn, turn_labels.get_child_count())
 	
-## Orders turns of all entities from highest Haste to lowest.
-## this can sort a list that's halfway through looping
-## eg a list that's normally [1,2,4,3]
+func validate_turn() -> void:
+	pass
+	# TODO implement this
+	# just scans through the turn order
+	# allows for one jump in haste
+	
+## Orders turns of all entities from highest Haste to lowest, 
+## while also preserving the current turn
+## eg: a list that's normally [1,2,4,3]
 ## looped around to [4,3,1,2]
 ## Will sort around to [3,4,1,2]
 func order_turns() -> void:
-	var index
+	var new_list = []
 	
-	for turn in turn_list:
-		for compare_turn in turn_list:
-			if compare_turn == turn:
-				continue
-			
-			if compare_turn.haste <= turn.haste:
-				index = turn_list.bsearch(turn)
-				turn_list.pop_at(index)
-				
-				index = turn_list.bsearch(compare_turn)
-				turn_list.insert(index, turn) # HACK this can get performance expensive with big lists
-				break
-				
-		index = turn_list.bsearch(turn)
-		turn_list.append(turn_list.pop_at(index))
-
+	if turn_list == []:
+		return
+	# if turn_list_ordered:
+		# return
+	
+	new_list = _turn_sublist(turn_list[0])
+	
+	var second_list = []
+	while turn_list != []:
+		
+		
+	new_list.append_array(second_list)
+	
+	self.turn_list = new_list
 
 func init_turns() -> void:
 	order_turns()
@@ -71,3 +79,23 @@ func init_turns() -> void:
 	# find the first biggest one
 	# if its in the first spot, just return
 	# else, split into two arrays,
+
+
+func _turn_sublist(cur_turn) -> Array[TileEntity]:
+	var new_list = []
+	var cur_next = cur_turn.haste
+	
+	while turn_list != []:
+		for turn in turn_list:
+			if turn.haste == cur_turn.haste:
+				turn_list.pop_at(turn_list.bsearch(turn))
+				new_list.append(turn)
+				
+			elif turn.haste < cur_next:
+				cur_next = turn.haste
+			
+		if cur_next == cur_turn.haste:
+			break
+	
+	return new_list
+#endregion
