f4176f677a64e28c2b9c8c1d26e4b43d758f484c
Clockknight <hello@tylerhanyinwang.com>
2026-03-04 02:11:56 -0800

    Unrefined info panel

--- scenes/events/hover_panel.gd
+++ scenes/events/hover_panel.gd
@@ -8,23 +8,33 @@ extends VBoxContainer
 
 
 func tile_hover(inc_tile : BattleTile = MouseHandler.hovered_tile):
+	#FIXME Panel back is now 100% Transparent
 	var entity:TileEntity = inc_tile.held_entity
 	if not entity:
 		return
-		
+
+	_define_sprite(entity)
+	
+	#PLANNED have these populate based on the entity
+	title.text = entity.character.o_name
+	subinfo.text = str(inc_tile.tile_position)
+	_define_description(entity)
+	
 	enable()
 
+func _define_description(_entity):
+	#TODO define the width based on the other elements' results
+	description.text = "You can see this thing."
+	
+func _define_sprite(entity):
+	#FIXME Sprite doesnt take the appropriate amount of space on the panel
 	var sprite = $Subject/Sprite
-		
 	if not sprite:
 		sprite = Sprite2D.new()
 		$Subject.add_child(sprite)
 		
 	sprite.sprite_frames = entity.sprite.sprite_frames.duplicate()
 	sprite.position = Vector2.ZERO
-	title.text = entity.character.o_name
-	subinfo.text = str(inc_tile.tile_position)
-	
 	
 
 func enable():
