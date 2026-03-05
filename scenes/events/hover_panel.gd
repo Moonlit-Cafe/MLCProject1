5c4c0df1bd46c494ea36273610b74b2b56820510
Clockknight <hello@tylerhanyinwang.com>
2026-03-05 14:49:35 -0800

    Give correct margins to sprite on hover_panel

--- scenes/events/hover_panel.gd
+++ scenes/events/hover_panel.gd
@@ -23,18 +23,18 @@ func tile_hover(inc_tile : BattleTile = MouseHandler.hovered_tile):
 	enable()
 
 func _define_description(_entity):
-	#TODO define the width based on the other elements' results
+	#TODO define the width of this control from the other elements' results
 	description.text = "You can see this thing."
 	
 func _define_sprite(entity):
-	#FIXME Sprite doesnt take the appropriate amount of space on the panel
-	var sprite = $Subject/Sprite
-	if not sprite:
-		sprite = Sprite2D.new()
-		$Subject.add_child(sprite)
-		
+	var sprite = $Subject/MarginContainer/Sprite
 	sprite.sprite_frames = entity.sprite.sprite_frames.duplicate()
 	sprite.position = Vector2.ZERO
+	
+	# HACK ugly, look for more elegant implementation
+	$Subject/MarginContainer.add_theme_constant_override("margin_right", sprite.sprite_frames.get_frame_texture(sprite.animation, 0).get_width()*2)
+	$Subject/MarginContainer.add_theme_constant_override("margin_bottom", sprite.sprite_frames.get_frame_texture(sprite.animation, 0).get_height()*2)
+
 	
 
 func enable():
