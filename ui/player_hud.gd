7dd5be398d9a69bc320c7844a1cb38ce77df7c22
Clockknight <hello@tylerhanyinwang.com>
2026-03-21 01:30:26 -0700

    Tweak random item generation on spawn

--- ui/player_hud.gd
+++ ui/player_hud.gd
@@ -92,15 +92,15 @@ func _generate_inventory() -> void:
 
 func _generate_random_itemnodes() -> void:
 	var rand_items : Array[Item] = []
-	for i in range(2):
-		rand_items.append(_random_item())
 	
 	for i in range(3):
+		rand_items.append(_random_item(ResourceManager.ItemType.MATERIAL))
 		rand_items.append(_random_item(ResourceManager.ItemType.EQUIPPABLE, i))
 	
+	rand_items.append(_random_item(ResourceManager.ItemType.EQUIPPABLE))
 	print(rand_items)
 		
-	for item in rand_items:
+	for item in rand_items:ii
 		var slot = container.get_child(randi_range(0, inv_size.x * inv_size.y - 1))
 		while slot.get_child_count() > 1:
 			slot = container.get_child(randi_range(0, inv_size.x * inv_size.y - 1))
@@ -117,16 +117,15 @@ func _generate_random_itemnodes() -> void:
 #endregion
 
 #region Helpers
-func _random_item(rand_type = null, item_count = null):
-	var idx = item_count
-	
+func _random_item(rand_type = null, item_id:int = -1, stack_count:int = 1):
 	if rand_type == null:
 		rand_type = randi_range(0, ResourceManager.ItemType.size()) as ResourceManager.ItemType
-	if item_count == null:
-		rand_type = ResourceManager.ItemType.MATERIAL
-		item_count = ResourceManager.get_data_count(ResourceManager.DataType.ITEM, rand_type)
-		idx = randi_range(0, item_count - 1)
-		print(idx)
+		
+	if item_id == -1:
+		item_id = ResourceManager.get_data_count(ResourceManager.DataType.ITEM, rand_type)
+		item_id = max(item_id-1, 0)
+		item_id = randi_range(0, item_id)
+	
 		
 	var type_str : String = ""
 	match(rand_type):
@@ -136,12 +135,15 @@ func _random_item(rand_type = null, item_count = null):
 			type_str = "USE_%s"
 		ResourceManager.ItemType.EQUIPPABLE:
 			type_str = "EQP_%s"
+			stack_count = 1
 		ResourceManager.ItemType.WEAPON:
 			type_str = "WEP_%s"
 		_:
-			type_str = "Null"
+			type_str = "NULL_"
 	
-	var item : Item = ResourceManager.item_compendium.get(type_str % idx)
+	var item : Item = ResourceManager.item_compendium.get(type_str % item_id)
+	if not item:
+		return _random_item()
 	print("Added Item: %s" % item.i_name)
 	return item
 	
