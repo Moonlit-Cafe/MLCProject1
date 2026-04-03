c564bcf9b41e263c919a37556483f9af2c524a65
Clockknight <hello@tylerhanyinwang.com>
2026-04-03 16:57:12 -0700

    Outline of map scene

---
+++ scenes/map_scene.gd
@@ -0,0 +1,38 @@
+extends BaseEventScene
+
+#region Declarations
+@onready var sections = $SectionContainer 
+#endregion
+
+#region Events
+func _ready() -> void:
+	_generate_sections()
+	_generate_stars()
+
+#endregion
+
+
+#region Helpers
+
+func _generate_sections() -> void:
+	var section_count = randi_range(5,11)
+	var new_section
+	
+	for i in range(0, section_count):
+		new_section = VBoxContainer.new()
+		sections.add_child(new_section)
+
+func _generate_stars() -> void:
+	var section_array = sections.get_children()
+	
+	for i in range(section_array.size(), 0):
+		var cur = section_array[i]
+		var prev = section_array[i+1]
+		
+		for j in range(1, randi_range(1,5)):
+			cur.add_child(_generate_star())
+			return
+	
+func _generate_star() -> Node:
+	return null
+#endregion
