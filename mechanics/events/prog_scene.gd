ed8e48f5c6612c55e5d4bc54aca7cf1ef5e41ffa
Clockknight <hello@tylerhanyinwang.com>
2026-04-17 10:22:51 -0700

    Tweak to testing progscene

--- mechanics/events/prog_scene.gd
+++ mechanics/events/prog_scene.gd
@@ -133,4 +133,6 @@ func _on_event_button_pressed(event: EventHolder) -> void:
 	
 	_clear_event_buttons()
 	button_container.get_parent().hide()
+	if test_version:
+		get_parent().get_parent().get_child(0).button_container.get_parent().hide()
 #endregion
