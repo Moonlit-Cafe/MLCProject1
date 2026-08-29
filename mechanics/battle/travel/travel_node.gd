eba31cd4cf2778cc4c6a9ea91ce92d27cdb4a909
Clockknight <hello@tylerhanyinwang.com>
2026-08-28 22:47:09 -0700

    Travel Skeleton

--- mechanics/battle/travel/travel_node.gd
+++ mechanics/battle/travel/travel_node.gd
@@ -1,1 +1,27 @@
 class_name TravelNode extends Button
+
+#region Declarations
+var connections : Array = [] :
+	set(new) :
+		var t : Node2D
+		var diff_vector : Vector2
+		
+		connections = new
+		clear_lines()
+		
+		for node in connections:
+			t = Line2D.new()
+			
+			diff_vector = node.global_position - self.global_position + Vector2.RIGHT * 100
+			t.points = [Vector2.ZERO, diff_vector]
+			
+			t.z_index += 10
+			add_child(t)
+#endregion
+
+#region Events
+func clear_lines() -> void:
+	for child in get_children():
+		if child is Line2D:
+			child.queue_free()
+#endregion
--- scenes/events/travel_scene.gd
+++ scenes/events/travel_scene.gd
@@ -14,7 +14,7 @@ var layers: Array[Array]
 var new_travel_node : PackedScene = preload("res://mechanics/battle/travel/travel_node.tscn")
 #endregion
 
-#region Declarations
+#region Events
 ## sets up zones on the planet given input data
 func _populate_section() -> void:
 	var layer_count : int
@@ -38,4 +38,30 @@ func _populate_section() -> void:
 			new_node.position = Vector2(X_INIT + X_GAP * i, Y_INIT + Y_GAP * j)
 			add_child(new_node)
 			new_node.name = 'Node %d, %d' % [i, j]
+			
+			
+	for i in range(0, layer_count - 1):
+		cur_layer = layers[i]
+		var next_layer : Array = layers[i]
+		var layer_width : int = cur_layer.size()
+		var next_width : int = next_layer.size()
+		
+		var l_index : int = 0
+		var r_index : int = randi_range(l_index, next_width)
+		
+		for j in range(0, layer_width):
+			var new_connections = next_layer.slice(l_index, r_index)
+			cur_layer[j].connections = new_connections
+			
+			l_index = randi_range(l_index, next_width)
+			if j == layer_width - 1:
+				r_index = next_width
+			else:
+				r_index = randi_range(l_index, next_width)
+				
+			
+	
+	# TYLER remember current travel layer
+		# modulate current layer node to yellow
+		# modulate all selectables to blue
 #endregion
